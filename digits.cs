b78177ddb50b023f34cc95be261d3b41466bef71
Jonathan <f.aracil.jonathan@gmail.com>
2020-11-10 21:20:42 +0100

    Cleaned digits exercise

--- digits.cs
+++ digits.cs
@@ -1,35 +1,36 @@
 using System;
 
+//Jonathan Aracil 1ºDAM Group 1
+
 class digits
 {
- static void Main()
- {
-//counters of number of digits a is for one digit, b is for 2 digits, c is for 3 digits and d is for more than 3 digits
-int a = 0, b = 0, c = 0, d = 0;
-Console.Write("Insert a number: ");
-// read the number as string to check the word "end"
-string e = Console.ReadLine();
-while(e!="end"){
-int input = Convert.ToInt32(e);
-if(input != 0)
-{
-if(input/10 == 0)
-a ++;  // it only has one digit
-else 
-if (input/100 == 0)
-b ++;  // the number has 2 digits
-else
-if (input/1000 == 0)
-c ++;  // the number has 3 digits
-else
-d++;  // the number has more than 3 digits
-}
-e = Console.ReadLine();
-}
-
-Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", a, b, c, d); 
-
-
- }
+	//The program consist in introduce numbers and show how much digits have each number
+	static void Main()
+	{
+		int oneDigit = 0, twoDigits = 0, threeDigits = 0, fourOrMoreDigits = 0;
+		Console.Write("Insert a number: ");
+		string mark = Console.ReadLine();
+		
+		while (mark != "end"){
+			int input = Convert.ToInt32(mark);
+			if (input != 0)
+			{	
+				if (input / 10 == 0)
+					oneDigit++;
+				else 
+				
+				if (input / 100 == 0)
+					twoDigits++;
+				else
+				
+				if (input / 1000 == 0)
+					threeDigits++;  
+				else
+					fourOrMoreDigits++;  
+			}
+		mark = Console.ReadLine();
+		}
+		Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", oneDigit, twoDigits, threeDigits, fourOrMoreDigits); 
+	}
 }
 
